22b95eedf7f88d7c3bd905d0ec2cf753d85692f5
Nicolas <rebelmedved@gmail.com>
2026-06-23 00:03:22 +0300

    Добавлена реализация класса TaskTracker с методами добавления задачи в список задач, вывода всех значений из списка, отметки задачи как выполненной, получения статуса задачи по переданному названию и вывода статистики по задачам в списке.

--- src/main/java/alfa/homework10/TaskTracker.java
+++ src/main/java/alfa/homework10/TaskTracker.java
@@ -1,4 +1,54 @@
 package alfa.homework10;
 
+import java.util.ArrayList;
+
 public class TaskTracker {
+    private ArrayList<Task> taskList;
+
+    public TaskTracker() {
+        taskList = new ArrayList<>();
+    }
+
+    public TaskTracker(ArrayList<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public void printTaskList() {
+        taskList.forEach(Task::printTaskInfo);
+    }
+
+    public void taskIsDone(String taskName) {
+        Task foundTask = getTaskFromListByTaskNameOrDefault(taskName);
+        int index = taskList.indexOf(foundTask);
+        if (foundTask.getTaskName() != null) {
+            foundTask.setDone(true);
+            taskList.set(index, foundTask);
+        }
+    }
+
+    public boolean isTaskDone(String taskName) {
+        Task foundTask = getTaskFromListByTaskNameOrDefault(taskName);
+        return foundTask.isDone();
+    }
+
+    public void printTasksStatistic() {
+        System.out.printf("""
+                ___
+                Всего задач: %d
+                Выполнено: %d
+                Открыто: %d
+                ___
+                """, taskList.size(),
+                taskList.stream().filter(Task::isDone).count(),
+                taskList.stream().filter(task -> !task.isDone()).count());
+    }
+
+    private Task getTaskFromListByTaskNameOrDefault(String taskName) {
+        return taskList.stream().filter(task -> taskName.equals(task.getTaskName())).findFirst()
+                .orElse(new Task());
+    }
 }
