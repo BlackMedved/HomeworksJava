6f8c521d19d9120bb0aef4a78de5174db8c85436
Nicolas <rebelmedved@gmail.com>
2026-06-23 00:19:03 +0300

    В классе Main добавлена проверка работы всех методов класса TaskTracker

--- src/main/java/alfa/homework10/Main.java
+++ src/main/java/alfa/homework10/Main.java
@@ -1,4 +1,21 @@
 package alfa.homework10;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+    static void main() {
+        ArrayList<Task> listOfTasks = new ArrayList<>();
+        Collections.addAll(listOfTasks, new Task("Проснуться пораньше"),
+                new Task("Выпить кофе"), new Task("Принять душ"));
+        TaskTracker taskTracker = new TaskTracker(listOfTasks);
+        taskTracker.printTaskList();
+        System.out.println("Задача \"Принять душ\" выполнена? " + (taskTracker.isTaskDone("Принять душ") ?
+                "Да" : "Нет"));
+        taskTracker.setTaskIsDone("Выпить кофе");
+        taskTracker.addTask(new Task("Собрать кровать", true));
+        taskTracker.setTaskIsDone("Принять душ");
+        taskTracker.printTaskList();
+        taskTracker.printTasksStatistic();
+    }
 }
