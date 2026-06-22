fbefb8f1b8ae1c4cc1a599c4a3911aafa46831ef
Nicolas <rebelmedved@gmail.com>
2026-06-22 23:00:08 +0300

    Добавлена функциональность в класс Task - поля taskName и isDone, конструкторы, геттеры и сеттеры и метод getTaskInfo, который возвращает информацию о задаче

--- src/main/java/alfa/homework10/Task.java
+++ src/main/java/alfa/homework10/Task.java
@@ -1,4 +1,37 @@
 package alfa.homework10;
 
 public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task() {}
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Task(String taskName, boolean isDone) {
+        this.taskName = taskName;
+        this.isDone = isDone;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void getTaskInfo() {
+        System.out.printf("[%s] %s", isDone ? "x" : " ", taskName);
+    }
 }
