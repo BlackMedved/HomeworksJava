e596b0aa70404d9bad0fac57c942b29cc01e1a94
Nicolas <rebelmedved@gmail.com>
2026-08-03 18:16:06 +0300

    Добалена функциональность в класс BoardGame и добавлен тест для нового метода canBeRentedBy

--- src/main/java/alfa/homework15/BoardGame.java
+++ src/main/java/alfa/homework15/BoardGame.java
@@ -1,4 +1,66 @@
 package alfa.homework15;
 
 public class BoardGame {
+    private String name;
+    private int minimalPlayerAge;
+    private double dayRentCost;
+    private boolean isRented;
+
+    public BoardGame() {
+        this.name = "без названия";
+        this.minimalPlayerAge = 0;
+        this.dayRentCost = 100;
+        this.isRented = false;
+    }
+
+    public BoardGame(String name, int minimalPlayerAge, double dayRentCost) {
+        if (name == null || name.isEmpty() || minimalPlayerAge < 0 || dayRentCost <=0)
+            throw new IllegalArgumentException();
+        this.name = name;
+        this.minimalPlayerAge = minimalPlayerAge;
+        this.dayRentCost = dayRentCost;
+        this.isRented = false;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardGame{" +
+                "name=" + name +
+                ", minimalPlayerAge='" + minimalPlayerAge + '\'' +
+                ", dayRentCost=" + dayRentCost +
+                ", isRented=" + isRented +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinimalPlayerAge() {
+        return minimalPlayerAge;
+    }
+
+    public void setMinimalPlayerAge(int minimalPlayerAge) {
+        this.minimalPlayerAge = minimalPlayerAge;
+    }
+
+    public double getDayRentCost() {
+        return dayRentCost;
+    }
+
+    public void setDayRentCost(double dayRentCost) {
+        this.dayRentCost = dayRentCost;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
 }
--- src/test/java/alfa/homework15/BoardGameTest.java
+++ src/test/java/alfa/homework15/BoardGameTest.java
@@ -1,8 +1,11 @@
 package alfa.homework15;
 
+import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,18 +27,18 @@ public class BoardGameTest {
                 () -> assertEquals(name, boardGame.getName(), "Некорректное название игры"),
                 () -> assertEquals(minimalPlayerAge, boardGame.getMinimalPlayerAge(), "Некорректный возраст"),
                 () -> assertEquals(dayRentCost, boardGame.getDayRentCost(), "Некорректное название игры"),
-                () -> assertEquals(false, boardGame.getIsRented(), "Некорректное значение брони")
+                () -> assertFalse(boardGame.isRented(), "Некорректное значение брони")
                 );
     }
 
     @ParameterizedTest
-    @CsvSource({
+    @CsvSource(value = {
             "null, 3, 200",
             ", 5, 300",
             "Уно, -2, 500",
             "Манчкин, 14, 0",
-            "Ведьмак, 16, -100"
-    })
+            "Ведьмак, 16, -100" },
+            nullValues = {"null"})
     public void errorWhileAddingIncorrectPropertiesInConstructor(String name, int minimalPlayerAge, double dayRentCost) {
         assertThrows(
                 IllegalArgumentException.class,
@@ -44,5 +47,38 @@ public class BoardGameTest {
         );
     }
 
+    @Test
+    public void boardGamePropertiesCorrectlyChangingWithGettersAndSetters() {
+        Faker faker = new Faker();
+        Random random = new Random();
+        BoardGame boardGame = new BoardGame();
+        String name = faker.boardgame().name();
+        int age = random.nextInt(0, 100);
+        double rent = random.nextInt(1, 5000) + Double.parseDouble(String.format("%.2f",
+                random.nextDouble()).replace(',', '.'));
+        boardGame.setName(name);
+        boardGame.setMinimalPlayerAge(age);
+        boardGame.setDayRentCost(rent);
+        assertAll("Проверка изменения параметров через геттеры и сеттеры",
+                () -> assertEquals(name, boardGame.getName(), "Некорректное название игры"),
+                () -> assertEquals(age, boardGame.getMinimalPlayerAge(), "Некорректный возраст"),
+                () -> assertEquals(rent, boardGame.getDayRentCost(), "Некорректное название игры"),
+                () -> assertFalse(boardGame.isRented(), "Некорректное значение брони")
+        );
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+            "16, 16, true",
+            "12, 25, true",
+            "0, 0, true",
+            "18, 17, false",
+            "6, 2, false"
+    })
+    public void canBeRentedByWorksCorrectAccordingToAgeChanging(int minimalPlayerAge, int age, boolean expectedResult) {
+        BoardGame boardGame = new BoardGame();
+        boardGame.setMinimalPlayerAge(minimalPlayerAge);
+        boolean canBeRantedByAge = boardGame.canBeRentedBy(age);
+        assertEquals(expectedResult, canBeRantedByAge, "Метод возвращает некорректное значение");
+    }
 }
